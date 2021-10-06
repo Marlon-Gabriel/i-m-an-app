81fad990e5a2d9394f63a6de932ff024fb1bd85b
Marlon-Gabriel <marlongabriel.floresramos.st@everis.nttdata.com>
2021-10-06 07:38:49 -0500

    Initial Commit

---
+++ I'm rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I'm rich
+//
+//  Created by Marlon Gabriel Flores Ramos on 6/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
